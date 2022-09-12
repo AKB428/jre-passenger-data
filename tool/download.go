c48ef3779efcc7a1838e23704275834f0eb29edb
AKB428 <gandara016+1@gmail.com>
2022-09-13 01:01:25 +0900

    add download

---
+++ tool/download.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	base := "https://www.jreast.co.jp/passenger/"
+
+	var url string
+	for i := 2000; i <= 2005; i++ {
+		if i == 2021 {
+			url = fmt.Sprintf("%s%s.html", base, "index")
+		} else {
+			url = fmt.Sprintf("%s%d.html", base, i)
+		}
+		fmt.Println(url)
+		download(url, fmt.Sprintf("%d.html", i))
+	}
+}
+
+func download(url, filename string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create("./htmls/" + filename)
+	if err != nil {
+		panic(err)
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		panic(err)
+	}
+}
