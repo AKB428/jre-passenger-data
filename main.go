836c702620c86788b5c2431098ca2adc6ab19673
AKB428 <gandara016+1@gmail.com>
2022-09-13 22:20:43 +0900

    add csv

--- main.go
+++ main.go
@@ -4,11 +4,25 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-func scrape(path string) {
+const endYear = 2021
+const startYear = 2020
+
+type record struct {
+	rank    string
+	station string
+	count   string
+}
+
+var stationList []string
+
+var stationMap map[string]map[int]int
+
+func scrape(path string, year int) {
 
 	f, err := os.Open(path)
 	if err != nil {
@@ -21,18 +35,63 @@ func scrape(path string) {
 		log.Fatal(err)
 	}
 
+	stationMap = make(map[string]map[int]int)
 	doc.Find("tbody > tr").Each(func(i int, s *goquery.Selection) {
 
-		title := s.Find("td.stationName").Text()
+		rank := s.Find("td:nth-child(1)").Text()
+		station := s.Find("td.stationName").Text()
 		count := s.Find("td:nth-child(5)").Text()
-		fmt.Printf("%d: %s %s\n", i, title, count)
+
+		if rank != "" {
+			//	fmt.Printf("%s %s %s\n", rank, station, count)
+			//r := record{rank, station, count}
+			if year == endYear {
+				stationList = append(stationList, station)
+				stationMap[station] = map[int]int{}
+			}
+
+			ci, _ := strconv.Atoi(count)
+			cs := stationMap[station]
+
+			if cs == nil {
+				cs = make(map[int]int)
+			}
+
+			cs[year] = ci
+			stationMap[station] = cs
+		}
+
 	})
 }
 
 func main() {
-	for i := 2021; i >= 2000; i-- {
+	for i := endYear; i >= startYear; i-- {
 		path := fmt.Sprintf("%s%d.html", "./htmls/", i)
-		fmt.Println(path)
-		scrape(path)
+		//fmt.Println(path)
+		scrape(path, i)
 	}
+	genCSV()
+}
+
+func genCSV() {
+	// 1行目を出力する
+	for _, v := range stationList {
+		fmt.Printf(",")
+		fmt.Print(v)
+	}
+	fmt.Println("")
+
+	for i := startYear; i <= endYear; i++ {
+		fmt.Print(i)
+		for _, v := range stationList {
+			fmt.Printf(",")
+
+			cs := stationMap[v]
+			v := cs[i]
+
+			fmt.Print(v)
+		}
+		fmt.Println("")
+	}
+
 }
