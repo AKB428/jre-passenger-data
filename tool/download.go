121bf3a12161374e7b1ca658a63f43b80b9172f0
AKB428 <gandara016+1@gmail.com>
2022-09-18 21:54:23 +0900

    fix

--- tool/download.go
+++ tool/download.go
@@ -3,41 +3,60 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
-func main() {
+const startYear = 2000
+const endYear = 2021
+const baseUrl = "https://www.jreast.co.jp/passenger/"
+const saveBaseDir = "htmls"
 
-	base := "https://www.jreast.co.jp/passenger/"
+func main() {
+	// 存在しなければカレントディレクトリに保存フォルダを作成する
+	checkSaveDir(saveBaseDir)
 
 	var url string
-	for i := 2000; i <= 2021; i++ {
-		if i == 2021 {
-			url = fmt.Sprintf("%s%s.html", base, "index")
+	for i := startYear; i <= endYear; i++ {
+		if i == endYear {
+			url = fmt.Sprintf("%s%s.html", baseUrl, "index")
 		} else {
-			url = fmt.Sprintf("%s%d.html", base, i)
+			url = fmt.Sprintf("%s%d.html", baseUrl, i)
 		}
 		fmt.Println(url)
 		download(url, fmt.Sprintf("%d.html", i))
 	}
 }
 
-func download(url, filename string) {
+func download(url, saveFilename string) {
 	resp, err := http.Get(url)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 
-	out, err := os.Create("./htmls/" + filename)
+	out, err := os.Create(filepath.Join(saveBaseDir, saveFilename))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer out.Close()
 
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+}
+
+func checkSaveDir(dir string) {
+	_, err := os.Stat(dir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.Mkdir(dir, 0755)
+			if err != nil {
+				panic(err)
+			}
+		}
 	}
 }
